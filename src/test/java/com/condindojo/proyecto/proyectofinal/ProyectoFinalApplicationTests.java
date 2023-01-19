c770880b478901a3f1d62716ce6679d7272ed9c1
rjfuentes99 <rjose.fmarin99@gmail.com>
2023-01-18 20:13:16 -0500

    Primer commit

---
+++ src/test/java/com/condindojo/proyecto/proyectofinal/ProyectoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.condindojo.proyecto.proyectofinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
